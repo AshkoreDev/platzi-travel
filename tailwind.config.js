bc63d751664c3b74580a95ab715a9d9281d0c7bb
Aracely Alba <aracelyalba8@gmail.com>
2022-01-28 00:15:38 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'LA': "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/LA.jpg')",
+        'bali': "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/bali.jpg')",
+        'chicago': "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/chicago.jpg')",
+        'europe': "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/europe.jpg')",
+        'iceland': "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/iceland.jpg')",
+        'miami': "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/miami.jpg')",
+        'newYork': "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/new_york.jpg')",
+        'norway': "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/norway.jpg')",
+        'sanFrancisco': "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/sanFrancisco.jpg')",
+        'sanFranciscoDesktop': "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/sanFranciscoDesktop.jpg')",
+        'seattle': "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/seattle.jpg')",
+        'switzerland': "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/switzerland.jpg')",
+        'sydney': "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/sydney.jpg')",
+        'yosemite': "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/yosemite.jpg')"
+      },
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'tertiary': '#61AEC9'
+      }),
+      textColor: {
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'tertiary': '#61AEC9'
+      },
+      fontFamily: {
+        Montserrat: ['Montserrat', 'san-serif']
+      }
+    },
+  },
+  variants: {
+    width: ["responsive", "hover", "focus"],
+    extend: {}
+  },
+  plugins: []
+}
